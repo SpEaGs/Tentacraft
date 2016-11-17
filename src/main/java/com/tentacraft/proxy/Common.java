587da638583cd740f48e139fcff6e84ce661d0b6
Colin <colinmcneil@gmail.com>
2016-11-16 21:55:34 -0500

    Added Working Proxies.

--- src/main/java/com/tentacraft/proxy/Common.java
+++ src/main/java/com/tentacraft/proxy/Common.java
@@ -1,19 +1,21 @@
 package com.tentacraft.proxy;
 
+import com.tentacraft.Tentacraft;
+
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class Common {
 	public void preInit(FMLPreInitializationEvent e) {
-
+		Tentacraft.log("Preinit");
     }
 
     public void init(FMLInitializationEvent e) {
-
+    	Tentacraft.log("Init");
     }
 
     public void postInit(FMLPostInitializationEvent e) {
-
+    	Tentacraft.log("PostInit");
     }
 }
