2e4be91fe03ef38714be90c7fa87445cc9574727
Colin <colinmcneil@gmail.com>
2016-11-16 21:32:02 -0500

    Added proxies

--- src/main/java/com/tentacraft/Tentacraft.java
+++ src/main/java/com/tentacraft/Tentacraft.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.tentacraft;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Tentacraft.MODID, version = Tentacraft.VERSION)
+public class Tentacraft
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "tentacraft";
     public static final String VERSION = "1.0";
     
     @EventHandler
@@ -17,4 +17,5 @@ public class ExampleMod
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
     }
+    
 }
