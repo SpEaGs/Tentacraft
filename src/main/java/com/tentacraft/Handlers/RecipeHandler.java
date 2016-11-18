2ebf85bf4a8344af582e1d2d34037d9967e13949
Zach Speagle <speags557k4@gmail.com>
2016-11-18 17:31:39 -0500

    Added BlockSquidChest crafting recipe

--- src/main/java/com/tentacraft/Handlers/RecipeHandler.java
+++ src/main/java/com/tentacraft/Handlers/RecipeHandler.java
@@ -1,8 +1,10 @@
 package com.tentacraft.Handlers;
 
 import com.tentacraft.Tentacraft;
+import com.tentacraft.blocks.RegisterBlocks;
 import com.tentacraft.items.RegisterItems;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -21,12 +23,15 @@ public class RecipeHandler
 		Tentacraft.log().info("Registering crafting recipes...");
 		//Squid Net (empty)
 		GameRegistry.addRecipe(new ItemStack(RegisterItems.ItemSquidNet), new Object[] {"SSS", "STS", "SSS", 'S', Items.STRING, 'T', RegisterItems.ItemTentacle});
+		//Squid Chest
+		GameRegistry.addRecipe(new ItemStack(RegisterBlocks.BlockSquidChest), new Object[] {"TTT", "TCT", "TTT", 'T', RegisterItems.ItemTentacle, 'C', Blocks.CHEST});
 		
 	}
 
 	public static void registerSmeltingRecipes()
 	{
 		Tentacraft.log().info("Registering smelting recipes...");
+		//Calamari
 		GameRegistry.addSmelting(RegisterItems.ItemTentacle, new ItemStack(RegisterItems.ItemCalamari, 1), 0.7f);
 	}
 
