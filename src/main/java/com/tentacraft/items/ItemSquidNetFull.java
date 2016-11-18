f0e9941186b26395529124514ca409722f6dd695
Zach Speagle <speags557k4@gmail.com>
2016-11-18 18:18:29 -0500

    Added code to full squid net

--- src/main/java/com/tentacraft/items/ItemSquidNetFull.java
+++ src/main/java/com/tentacraft/items/ItemSquidNetFull.java
@@ -1,9 +1,15 @@
 package com.tentacraft.items;
 
-import com.tentacraft.Ref;
-import com.tentacraft.Tentacraft;
-
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemSquidNetFull extends Item
 {
@@ -11,6 +17,24 @@ public class ItemSquidNetFull extends Item
 	public ItemSquidNetFull()
 	{
 		maxStackSize = 1;
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand,
+									  EnumFacing facing, float hitX, float hitY, float hitZ)
+	{
+		if(!world.isRemote)
+		{
+			Entity squid = new EntitySquid(world);
+			squid.posX = hitX;
+			squid.posY = hitY;
+			squid.posZ = hitZ;
+			world.spawnEntityInWorld(squid);
+			int slot = player.inventory.currentItem;
+			player.inventory.setInventorySlotContents(slot, new ItemStack(RegisterItems.ItemSquidNet,1));
+			return EnumActionResult.SUCCESS;
+		}
+		return EnumActionResult.FAIL;
 	}
 	
 }
